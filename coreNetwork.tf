1658e604af8ad5281d09d41d2c3748cacc86c761
OmarKallelPro <omar.kallel.pro@gmail.com>
2020-11-18 12:16:15 +0100

    CoreNetwork added

--- coreNetwork.tf
+++ coreNetwork.tf
@@ -9,7 +9,7 @@ resource "azurerm_public_ip" "vpnGatewayPublicIp" {
     resource_group_name           = azurerm_resource_group.core.name  
     location                      = azurerm_resource_group.core.location
     tags                          = azurerm_resource_group.core.tags
-    public_ip_address_allocation  = "dynamic"
+    allocation_method             = "Dynamic"
 }
 
 resource "azurerm_virtual_network" "core" {
@@ -25,36 +25,36 @@ resource "azurerm_subnet" "gatewaysubnet" {
     name  = "gatewaysubnet"
     resource_group_name = azurerm_resource_group.core.name
     virtual_network_name = azurerm_virtual_network.core.name
-    address_prefix       = "10.0.0.0/24"
+    address_prefixes       = [ "10.0.0.0/24" ]
 }
 
 resource "azurerm_subnet" "training" {
     name  = "training"
     resource_group_name = azurerm_resource_group.core.name
     virtual_network_name = azurerm_virtual_network.core.name
-    address_prefix       = "10.0.1.0/24"
+    address_prefixes       = [ "10.0.1.0/24" ]
 }
 
 resource "azurerm_subnet" "dev" {
     name  = "dev"
     resource_group_name = azurerm_resource_group.core.name
     virtual_network_name = azurerm_virtual_network.core.name
-    address_prefix       = "10.0.2.0/24"
+    address_prefixes       = [ "10.0.2.0/24" ]
 }
 
 # resource "azurerm_virtual_network_gateway" "vpngateway" {
 #     name = "vpngateway"
 #     location = azurerm_resource_group.core.location
-#     resource_group = azurerm_resource_group.core.name
+#     resource_group_name = azurerm_resource_group.core.name
 #     tags = azurerm_resource_group.core.tags
 #     type = "Vpn"
 #     vpn_type = "RouteBased"
 #     sku = "Basic"
-#     enable_dbg = true
+#     enable_bgp = true
 #     ip_configuration {
 #       name = "vpnGwConfig"
-#       public_ip_address_id = azurerm_public_ip.vpnGatewayPublicIp.Id
+#       public_ip_address_id = azurerm_public_ip.vpnGatewayPublicIp.id
 #       private_ip_address_allocation = "Dynamic"
-#       subnet_id = azurerm_subnet.gatewaysubnet.Id
+#       subnet_id = azurerm_subnet.gatewaysubnet.id
 #     }
 # }
